83c84068040829660f9674c26baba8aea0708d9b
Charles Bretton <nyasura16@gamil.com>
2023-05-25 14:35:51 +0200

    dossier créé avec un début de fichier php

---
+++ chatbot-plugin/chatbot-plugin.php
@@ -0,0 +1,16 @@
+/*
+ * Plugin Name:       Chatbot Plugin
+ * Plugin URI:        
+ * Description:       Chats about  neuroscience using an AI
+ * Version:           1.0
+ * Requires at least: 
+ * Requires PHP:      7.2
+ * Authors:           Clemence Millord, Charles Bretton, Liam Flandrinck
+ * Author URI:        
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        
+ * Text Domain:       chatbot-plugin
+ * Domain Path:       /languages
+ */
+ 
