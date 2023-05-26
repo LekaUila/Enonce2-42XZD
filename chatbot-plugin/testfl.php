0993d501efc9597a3dd546bf41b54bc9a5726e21
unknown <liam.flandrinck.58@gmail.com>
2023-05-26 10:23:45 +0200

    connection to api ok, not functionnel, but it maybe ok

--- chatbot-plugin/testfl.php
+++ chatbot-plugin/testfl.php
@@ -7,27 +7,33 @@ echo "your prompt is \"" . $prompt . "\". You are rick rolled !";
 
 $ch = curl_init();
 
-$OPENAI_API_KEY = "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";
+$OPENAI_API_KEY = "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";
 
 //url
-curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/models");
+curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/chat/completions");
 //header
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Cache-Control: no-cache",
     "content-type:application/json;charset=utf-8",
     "Authorization: Bearer " . $OPENAI_API_KEY,
     "OpenAI-Organization: org-Qn8QMygcPCH9XFx5I15vYgVG",
-    "model: gpt-3.5-turbo",
-    "messages"=> [array("role"=> "user", "content"=> "Say this is a test!")]
+
 
 ));
 /*
 //POST
 curl_setopt($ch, CURLOPT_POST, true);
 //request content
-$datas = array("user"=>$prompt);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+
 */
+$datas = array( "model" => "gpt-3.5-turbo",
+                "messages"=> array(  
+                                    "content: What day of the wek is it?",
+                                    "role: user"));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datas));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
 $response = curl_exec($ch);
 
 echo $response;
