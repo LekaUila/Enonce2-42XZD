1f8a54934497fe47bfced258bc8d16dca07bb2e1
unknown <liam.flandrinck.58@gmail.com>
2023-05-26 07:43:21 +0200

    sorry, now, the testfl.php file can recive correctly the promt

--- chatbot-plugin/chatbot-plugin.php
+++ chatbot-plugin/chatbot-plugin.php
@@ -20,137 +20,139 @@ function create_chat()
 
 
     <div id="global_div" style="margin: auto; text-align: center;">
-        <div id="msg_div" style="
-        margin: auto; width: 80%;
-        border: solid #080708; padding: 1%; text-align: left; scroll-behavior: auto;
-        overflow-y: scroll;
-        height: 600px;
-        ">
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--                                      JAVASCRIPT                                   -->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-            <script>
+    <div id="msg_div" style="
+    margin: auto; width: 80%;
+    border: solid #080708; padding: 1%; text-align: left; scroll-behavior: auto;
+    overflow-y: scroll;
+    height: 600px;
+    ">
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--                                      JAVASCRIPT                                   -->
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--###################################################################################-->
+        <script>
 
-                function addElement (user, text, backgroundColor) {
-                    console.log("addElement launch");
-                // crée un nouvel élément div
-                var newDiv = document.createElement("div");
-                // et lui donne un peu de contenu
-                var newContent = document.createTextNode(user + " : " + text);
-                // ajoute le nœud texte au nouveau div créé
-                newDiv.appendChild(newContent);
+            function addElement (user, text, backgroundColor) {
+                console.log("addElement launch");
+            // crée un nouvel élément div
+            var newDiv = document.createElement("div");
+            // et lui donne un peu de contenu
+            var newContent = document.createTextNode(user + " : " + text);
+            // ajoute le nœud texte au nouveau div créé
+            newDiv.appendChild(newContent);
 
-                // ajoute le nouvel élément créé et son contenu dans le DOM
-                var currentDiv = document.getElementById("after_msg_div");
-                var currentDivcontainer = document.getElementById("msg_div");
-                currentDivcontainer.insertBefore(newDiv, currentDiv);
+            // ajoute le nouvel élément créé et son contenu dans le DOM
+            var currentDiv = document.getElementById("after_msg_div");
+            var currentDivcontainer = document.getElementById("msg_div");
+            currentDivcontainer.insertBefore(newDiv, currentDiv);
 
-                newDiv.style.border = "black solid 1px";
-                newDiv.style.color = "white";
-                newDiv.style.backgroundColor = backgroundColor;
-                newDiv.style.borderRadius = "30px";
-                newDiv.style.marginTop = "1%";
-                newDiv.style.marginBottom = "1%";
-                newDiv.style.padding = "1%";
-                newDiv.style.paddingLeft = "2%";
-                newDiv.style.paddingRight = "2%";
-                }
+            newDiv.style.border = "black solid 1px";
+            newDiv.style.color = "white";
+            newDiv.style.backgroundColor = backgroundColor;
+            newDiv.style.borderRadius = "30px";
+            newDiv.style.marginTop = "1%";
+            newDiv.style.marginBottom = "1%";
+            newDiv.style.padding = "1%";
+            newDiv.style.paddingLeft = "2%";
+            newDiv.style.paddingRight = "2%";
+            }
 
-                function print_user_message() {
-                    console.log("print_user_message launch");
-                    var text_input = document.getElementById("input_user");
-                    var txt = text_input.value;
-                    text_input.value = "";
-                    addElement("you", txt, "#3772FF");
-                }
-                function print_ia_message() {
-                    console.log("print_ia_message launch");
-                    var text_input = document.getElementById("input_user");
-                    var txt = text_input.value;
-                    
-
-
-                    //Fichier PHP script.php
-                    //Code JavaScript
-                    var requete = new XMLHttpRequest();
-                    requete.open("POST", "wp-content/plugins/testfl.php");
-                    requete.send("prompt=" + txt);
-                    requete.onload = function() {
-                    //La variable à passer est alors contenue dans l\'objet response et l\'attribut responseText.
-                    var variableARecuperee = this.responseText;
-                    addElement("AI", variableARecuperee, "#DF2935");
-                    };
-                    //requete.open(get, script.php, true); //True pour que l\'exécution du script continue pendant le chargement, false pour attendre.
-                    //requete.send();
+            function print_user_message() {
+                console.log("print_user_message launch");
+                var text_input = document.getElementById("input_user");
+                var txt = text_input.value;
+                addElement("you", txt, "#3772FF");
+            }
+            function print_ia_message() {
+                console.log("print_ia_message launch");
+                var text_input = document.getElementById("input_user");
+                var txt = text_input.value;
+                
 
 
-                    //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
+                //Fichier PHP script.php
+                //Code JavaScript
+                var requete = new XMLHttpRequest();
+                requete.open("POST", "wp-content/plugins/testfl.php");
+                requete.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                requete.send("prompt=" + txt);
+                requete.onload = function() {
+                //La variable à passer est alors contenue dans l\'objet response et l\'attribut responseText.
+                var variableARecuperee = this.responseText;
+                addElement("AI", variableARecuperee, "#DF2935");
+                };
+                //requete.open(get, script.php, true); //True pour que l\'exécution du script continue pendant le chargement, false pour attendre.
+                //requete.send();
 
 
-                }
-                function scroll_bottom(params) {
-                    console.log("scroll_bottom launch");
-                    element = document.getElementById("msg_div");
-                    element.scrollTop = element.scrollHeight;
-                }
-
-                function send_rick_roll ()
-                {
-                    console.log("addElement launch");
-                // crée un nouvel élément div
-                var newDiv = document.createElement("div");
-                // et lui donne un peu de contenu
-                newDiv.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
-                // ajoute le nouvel élément créé et son contenu dans le DOM
-                var currentDiv = document.getElementById("after_msg_div");
-                var currentDivcontainer = document.getElementById("msg_div");
-                currentDivcontainer.insertBefore(newDiv, currentDiv);
-                /*
-                newDiv.style.width = "560";
-                newDiv.style.height = "315";
-                newDiv.title = "YouTube video player";
-                newDiv.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
-                //frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen
-                */
-                }
-
-                function send_message() {
-                    console.log("send_message launch");
-                    print_user_message();
-                    print_ia_message();
-                    send_rick_roll();
-                    scroll_bottom();
-                }
+                //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
 
 
-                window.addEventListener("load", function () {
-                    
+            }
+            function scroll_bottom(params) {
+                console.log("scroll_bottom launch");
+                element = document.getElementById("msg_div");
+                element.scrollTop = element.scrollHeight;
+            }
 
-                console.log("Cette fonction est exécutée une fois quand la page est chargée.");
-                });
-            </script>
-            <div id="after_msg_div"></div>
-        </div>
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--                                      INTERFACE                                   -->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
-    <!--###################################################################################-->
+            function send_rick_roll ()
+            {
+                console.log("addElement launch");
+            // crée un nouvel élément div
+            var newDiv = document.createElement("div");
+            // et lui donne un peu de contenu
+            newDiv.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+            // ajoute le nouvel élément créé et son contenu dans le DOM
+            var currentDiv = document.getElementById("after_msg_div");
+            var currentDivcontainer = document.getElementById("msg_div");
+            currentDivcontainer.insertBefore(newDiv, currentDiv);
+            /*
+            newDiv.style.width = "560";
+            newDiv.style.height = "315";
+            newDiv.title = "YouTube video player";
+            newDiv.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+            //frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen
+            */
+            }
+
+            function send_message() {
+                console.log("send_message launch");
+                print_user_message();
+                print_ia_message();
+                send_rick_roll();
+                var text_input = document.getElementById("input_user");
+                text_input.value = "";
+                scroll_bottom();
+            }
+
+
+            window.addEventListener("load", function () {
+                
+
+            console.log("Cette fonction est exécutée une fois quand la page est chargée.");
+            });
+        </script>
+        <div id="after_msg_div"></div>
+    </div>
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--                                      INTERFACE                                   -->
+<!--###################################################################################-->
+<!--###################################################################################-->
+<!--###################################################################################-->
 
 
 
-        <div id="input_div" style="margin: auto; text-align: right; width: 82%; padding: 0%; border: solid #080708; background-color: #F1BF98;">
+    <div id="input_div" style="margin: auto; text-align: right; width: 82%; padding: 0%; border: solid #080708; background-color: #F1BF98;">
 
-            <input type="text" placeholder="enter your question" id="input_user" style="border: none; background-color: #F1BF98; display: inline-block; width: 80%; margin: 0%;"><button onclick="send_message()" style="border: none; display: inline-block; width: 18%; margin: 0%; background-color: #DF2935;">Send</button> 
-    
-        </div>
-l
+        <input type="text" placeholder="enter your question" id="input_user" style="border: none; background-color: #F1BF98; display: inline-block; width: 80%; margin: 0%;"><button onclick="send_message()" style="border: none; display: inline-block; width: 18%; margin: 0%; background-color: #DF2935;">Send</button> 
+
+    </div>
+
     ';
  }
 
--- chatbot-plugin/testfl.php
+++ chatbot-plugin/testfl.php
@@ -3,6 +3,5 @@ $prompt = "test";
 foreach ($_POST as $key => $value) {
     $prompt = $value;
 }
-
 echo "your prompt is \"" . $prompt . "\". You are rick rolled !";
 ?>
